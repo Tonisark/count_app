f4efa1a7dc9a4b2490c31ac0901cf0793f8bfcca
Toni2stark <sogoaheadistill@gmail.com>
2021-07-25 13:40:33 +0430

    count-app

---
+++ app/src/main/java/com/example/testpro/MainActivity.java
@@ -0,0 +1,87 @@
+/** created bt Toni stark/Toni sasrk */
+package com.example.testpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText correctyear,Birthyear ;
+    TextView textView;
+    Button Counter;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+
+        Counter = findViewById(R.id.Counter1);
+        correctyear = findViewById(R.id.correct_year);
+        Birthyear = findViewById(R.id.Birth_year);
+
+
+
+
+
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                int k = Integer.parseInt(correctyear.getText().toString());
+                int e = Integer.parseInt(Birthyear.getText().toString());
+                int age=  k-e  ;
+                textView.setText(age);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+
+            }
+        });
+
+        Counter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                int k = Integer.parseInt(correctyear.getText().toString());
+                int e= Integer.parseInt(Birthyear.getText().toString());
+
+
+
+                if (k == e)
+                {
+                    Toast.makeText(MainActivity.this, "Please don't write same year", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
